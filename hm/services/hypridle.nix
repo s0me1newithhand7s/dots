baa4a44fecb2a1c20fd0caf58b9d7bf598b40e7e
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-07-14 19:01:20 +0300

    Update hypridle.nix

--- hm/services/hypridle.nix
+++ hm/services/hypridle.nix
@@ -10,24 +10,26 @@
                     before_sleep_cmd = "loginctl lock-session";
                     after_sleep_cmd = "hyprctl dispatch dpms on";
                 };
-                listener ={
+                listener = [
+                {
                     timeout = 300;
                     on-timeout = "brightnessctl -s set 10";
                     on-resume = "brightnessctl -r";
-                };
-                listener ={
+                }
+                {
                     timeout = 600;
                     on-timeout = "hyprlock --immediate";
-                };
-                listener ={
+                }
+                {
                     timeout = 630;
                     on-timeout = "hyprctl dispatch dpms off";
                     on-resume = "hyprctl dispatch dpms on";
-                };
-                listener ={
+                }
+                {
                     timeout = 900;
                     on-timeout = "systemctl suspend";
-                };
+                }
+                ];
             };
         };
     };
